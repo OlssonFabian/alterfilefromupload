eabb3e18e1f4d4e20548d25a4b8b5957f4aa3b02
Fabian Olsson <fabtasticdesign@live.se>
2020-10-06 12:32:54 +0200

    frontend that might work

--- src/components/FileUploadComponent.vue
+++ src/components/FileUploadComponent.vue
@@ -1,21 +1,49 @@
 <template>
     <v-app>
-    <v-container class="grey lighten-7">
-        <h1>Hello World</h1>
-        <v-btn
-      class="mx-2"
-      fab
-      dark
-      small
-      color="pink"
-    >
-        </v-btn>
+    <v-container class="grey lighten-5">
+        <v-row justify="space-around">
+            <v-card width="70vw" center>
+                <v-file-input
+                    accept="text/*"
+                    label="File input"
+                    value="textFile"
+                    v-model="textFile"
+                ></v-file-input>
+                <v-btn color="primary" text @click="uploadFile()">upload</v-btn>
+            </v-card>
+        </v-row>
     </v-container>
     </v-app>
 </template>
 
 <script>
     export default {
+        mounted() {
+            console.log('Component mounted.')
+        },
+        data() {
+            return {
+                textFile: '',
+                uploadedFile: {},
+            }
+        },
+        methods: {
+            uploadFile: function(textFile){
+                const uploadedFile = new Blob([this.textFile], { type: "text/xml"});
+                let currentObj = this;
 
+                const config = {
+                    headers: { 'content-type': 'text/xml' }
+                }
+
+                axios.post('/fileSubmit', uploadedFile, config)
+                .then(function (response) {
+                    currentObj.success = response.data.success;
+                })
+                .catch(function (error) {
+                    currentObj.output = error;
+                });
+            }
+        }
     }
 </script>
