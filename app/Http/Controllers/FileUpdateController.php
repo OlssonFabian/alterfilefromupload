019e208be5f2e25c020af4e3ea14a54aeee63bf2
Fabian Olsson <fabtasticdesign@live.se>
2020-10-07 11:44:56 +0200

    not really working yet, no content returned?

--- app/Http/Controllers/FileUpdateController.php
+++ app/Http/Controllers/FileUpdateController.php
@@ -7,27 +7,31 @@ use Illuminate\Support\Facades\Storage;
 
 class FileUpdateController extends Controller
 {
+    public static $folder = 'textfiles';
+    public static $disk = 'public';
     public static function fileUpdate($file){
-        $filePath = '/textfiles//' . $file;
-        $content = Storage::disk('public')->get($filePath);
+        $filePath = self::$folder . '/' . $file;
+        //$upload_path = Storage::disk('public')->path('/textfiles' )
+        $content = Storage::disk(self::$disk)->get($filePath);
+        $updatedContent = $content;
         //$content = Storage::disk('uploads')->get($path);
 
         //seperates each word to an array item
-        $arrayOfWords = str_word_count($content, 1, 'àáãç3');
 
-        //finds out how many times a word is occuring
-        $wordCount = array_count_values($arrayOfWords);
+        // //finds out how many times a word is occuring
+        // $wordCount = array_count_values(str_word_count($content, 1, 'àáãç3'));
 
-        //finds the highest number
-        $value = max($wordCount);
+        // //finds the highest number
+        // $value = max($wordCount);
 
-        //finds the key associated with the highest number
-        $key = array_search($value, $wordCount);
-        //the replacing value
-        $fooBarKey =  'foo' . $key . 'bar';
+        // //finds the key associated with the highest number
+        // $key = array_search($value, $wordCount);
 
-        //updates the content
-        $updatedContent = $content->preg_replace($key, $foobarKey);
+        // //the replacing value
+        // $fooBarKey =  'foo' . $key . 'bar';
+
+        // //updates the content
+        // $updatedContent = $content->preg_replace($key, $foobarKey);
 
         return $updatedContent;
     }
