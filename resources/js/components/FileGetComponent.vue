6438fde07d002e839be6e4f2ff6970885b3dd8a7
Fabian Olsson <fabtasticdesign@live.se>
2020-10-07 19:27:06 +0200

    assoc array instead of normal array

--- resources/js/components/FileGetComponent.vue
+++ resources/js/components/FileGetComponent.vue
@@ -3,9 +3,15 @@
     <v-container class="grey lighten-5">
         <v-row justify="space-around">
             <v-card width="70vw" center>
-                <p>Here be ccontent</p>
-                <v-btn color="primary" text @click="getFileContent">upload</v-btn>
+                <v-btn color="primary" text @click="getFileContent">Get altered files</v-btn>
             </v-card>
+            <ul>
+                <li v-for="fileWithContent in filesWithContents>
+                    <v-card>
+                        {{fileWithContent}}
+                    </v-card>
+                </li>
+            </ul>
         </v-row>
     </v-container>
     </v-app>
@@ -15,16 +21,14 @@
     export default {
         data() {
             return {
-                content: '',
+                filesWithContents: [],
             }
         },
         methods: {
             getFileContent: function(){
 
                 axios.get('/getTextFile')
-                .then(function (response) {
-                    console.log(response);
-                })
+                .then(response => (this.filesWithContents = response))
                 .catch(function (error) {
                     console.log('something went wrong')
                 });
