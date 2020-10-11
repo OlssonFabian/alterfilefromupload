5219a11bd92f94a7c249cb1c99a23e7184aaf396
Fabian Olsson <fabtasticdesign@live.se>
2020-10-11 12:36:21 +0200

    some refactoring

--- app/Http/Controllers/FileUpdateController.php
+++ app/Http/Controllers/FileUpdateController.php
@@ -12,20 +12,25 @@ class FileUpdateController extends Controller
     public static $regExAll = 'aàȁáâǎãāăȃȧäåẚảạḁąᶏậặầằắấǻẫẵǡǟẩẳⱥæǽǣᴂꬱꜳꜵꜷꜹꜻꜽɐɑꭤᶐꬰɒͣᵃªᵄᵆᵅᶛᴬᴭᴀᴁₐbḃƅƀᵬɓƃḅḇᶀꞗȸßẞꞵꞛꞝᵇᵝᴮᴯʙᴃᵦcćĉčċƈçḉɕꞔꞓȼ¢ʗᴐᴒɔꜿᶗꝢꝣ©ͨᶜᶝᵓᴄdďḋᵭðđɗᶑḓḍḏḑᶁɖȡꝱǳʣǆʤʥȸǲǅꝺẟƍƌͩᵈᶞᵟᴰᴅᴆeèȅéēêěȇĕẽėëẻḙḛẹȩęᶒⱸệḝềḕếḗễểɇəǝɘɚᶕꬲꬳꬴᴔꭁꭂ•ꜫɛᶓȝꜣꝫɜᴈᶔɝɞƩͤᵉᵊᵋᵌᶟᴱᴲᴇⱻₑₔfẜẝƒꬵḟẛᶂᵮꞙꝭꝼʩꟻﬀﬁﬂﬃﬄᶠꜰgǵḡĝǧğġģǥꬶᵷɡᶃɠꞡᵍᶢᴳɢʛhħĥȟḣḧɦɧḫḥẖḩⱨꜧꞕƕɥʮʯͪʰʱꭜᶣᵸꟸᴴʜₕiìȉíīĩîǐȋĭïỉɨḭịįᶖḯıɩɪꭠꭡᴉᵻᵼĳỻİꟾꟷͥⁱᶤᶦᵎᶧᶥᴵᵢjȷĵǰɉɟʝĳʲᶡᶨᴶᴊⱼkḱǩꝁꝃꝅƙḳḵⱪķᶄꞣʞĸᵏᴷᴋₖlĺľŀłꝉƚⱡɫꬷꬸɬꬹḽḷḻļɭȴᶅꝲḹꞎꝇꞁỻǈǉʪʫɮˡᶩᶪꭝꭞᶫᴸʟᴌₗmḿṁᵯṃɱᶆꝳꬺꭑᴟɯɰꟺꟿꟽͫᵐᶬᶭᴹᴍₘnǹńñňŉṅᵰṇṉṋņŋɳɲƞꬻꬼȵᶇꝴꞃꞑꞥᴝᴞǋǌⁿᵑᶯᶮᶰᴺᴻɴᴎₙoᴏᴑòȍóǿőōõôȏǒŏȯöỏơꝍọǫⱺꝋɵøᴓǭộợồṑờốṍṓớỗỡṏȭȱȫổởœɶƣɸƍꝏʘꬽꬾꬿꭀꭁꭂꭃꭄꭢꭣ∅ͦᵒᶱºꟹᶲᴼᴽₒpṕṗꝕꝓᵽᵱᶈꝑþꝥꝧƥƪƿȹꟼᵖᴾᴘᴩᵨₚqʠɋꝙꝗȹꞯʘθᶿrŕȑřȓṙɍᵲꝵꞧṛŗṟᶉꞅɼɽṝɾᵳᴦɿſⱹɹɺɻ®ꝶꭇꭈꭉꭊꭋꭌͬʳʶʴʵᴿʀʁᴙᴚꭆᵣsśŝšṡᵴꞩṣşșȿʂᶊṩṥṧƨʃʄʆᶋᶘꭍʅƪﬅﬆˢᶳᶴꜱₛtťṫẗƭⱦᵵŧꝷṱṯṭţƫʈțȶʇꞇꜩʦʧʨᵺͭᵗᶵᵀᴛₜuùȕúűūũûǔȗŭüůủưꭒʉꞹṷṵụṳųᶙɥựǜừṹǘứǚữṻǖửʊᵫᵿꭎꭏꭐꭑͧᵘᶶᶷᵙᶸꭟᵁᴜᵾᵤvṽⱱⱴꝟṿᶌʋʌͮᵛⱽᶹᶺᴠᵥwẁẃŵẇẅẘⱳẉꝡɯɰꟽꟿʍʬꞶꞷʷᵚᶭᵂᴡxẋẍᶍ×ꭓꭔꭕꭖꭗꭘꭙˣ˟ᵡₓᵪyỳýȳỹŷẏÿẙỷƴɏꭚỵỿɣɤꝩʎƛ¥ʸˠᵞʏᵧzźẑžżƶᵶẓẕʐᶎʑȥⱬɀʒǯʓƺᶚƹꝣᵹᶻᶼᶽᶾᴢᴣ';
 
     public static function fileUpdate($file, $name){
-        $filePath = self::$folder . '/' . $name;
-        $upload_path = Storage::disk('public')->path('textfiles' ) . '/' . $name;
 
-        $content = file_get_contents( $upload_path );
-        $wordCountsOriginal = array_count_values(str_word_count($content, 1, self::$regExAll));
+        $content = file_get_contents( Storage::disk('public')->path('textfiles' ) . '/' . $name );
+        //Content string
 
-        $wordCountsLowercase = str_word_count($content, 1, self::$regExAll);
-        $wordCountsLowercase = array_change_key_case($wordCountsLowercase, CASE_LOWER);
-        $wordCountsLowercase = array_count_values($wordCountsLowercase);
+        $wordArray = str_word_count($content, 1, self::$regExAll);
+        //[0]=>Tjenare [1]=>ord [2]=>annatord [3]=>tjenare,
 
-        $words = array_keys($wordCountsOriginal, max($wordCountsLowercase));
+        $wordCountsLowercase = array_count_values(str_word_count(strtolower($content), 1, self::$regExAll));
+        //['Tjenare']=>5
+
+        //$wordCountsLowercase = str_word_count($content, 1, self::$regExAll);
+        //$wordCountsLowercase = array_change_key_case($wordCountsOriginal, CASE_LOWER);
+        //$wordCountsLowercase = array_map('strtolower', );
+        //$wordCountsLowercase = array_change_key_case(array_count_values($wordCountsOriginal),CASE_LOWER);
+
+        $words = array_keys($wordArray, max($wordCountsLowercase));
         foreach ($words as $word) {
-                $fooBarWord =  'foo' . $word . 'bar';
-                $content = preg_replace( '/' . $word . '/', $fooBarWord, $content);
+                $content = str_ireplace($word, 'foo' . $word . 'bar', $content);
+                //$content = $word;
         }
         return $content;
     }
