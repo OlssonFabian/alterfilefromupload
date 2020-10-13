afca024c9920132e513c312c0581d63ca18720a7
Fabian Olsson <fabtasticdesign@live.se>
2020-10-13 15:37:39 +0200

    gets rid of non UTF8 chars

--- app/Http/Controllers/FileGetController.php
+++ app/Http/Controllers/FileGetController.php
@@ -15,7 +15,7 @@ class FileGetController extends Controller
             if (strpos($file, '-user-id-' .  Auth::user()->id) !== false) {
                 $content = file_get_contents($file);
                 $fileName = basename($file);
-                array_push($userFileList, ['title' => $fileName, 'content'=> $content]);
+                array_push($userFileList, mb_convert_encoding(['title' => $fileName, 'content'=> $content], 'UTF-8', 'UTF-8'));
             }
         }
         return response()->json([
